abc03333d42880823e3f26bea6712cea1aba5e8a
fqchen <991glasses@163.com>
2024-08-22 00:32:07 +0800

    1.改进了生成简报的prompt；2.增加了智谱大模型

--- src/llm.py
+++ src/llm.py
@@ -1,18 +1,21 @@
 import os
 from openai import OpenAI  # 导入OpenAI库用于访问GPT模型
+from zhipuai import ZhipuAI # 导入智谱AI库用于访问智谱AI模型
 from logger import LOG  # 导入日志模块
 
 class LLM:
     def __init__(self):
         # 创建一个OpenAI客户端实例
-        self.client = OpenAI()
+        self.openai_client = OpenAI()
+        self.zhipu_client = ZhipuAI(api_key=os.getenv('ZHIPU_API_KEY')) 
         # 配置日志文件，当文件大小达到1MB时自动轮转，日志级别为DEBUG
         LOG.add("daily_progress/llm_logs.log", rotation="1 MB", level="DEBUG")
 
     def generate_daily_report(self, markdown_content, dry_run=False):
         # 构建一个用于生成报告的提示文本，要求生成的报告包含新增功能、主要改进和问题修复
-        prompt = f"以下是项目的最新进展，根据功能合并同类项，形成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题；:\n\n{markdown_content}"
+        system_prompt = "以下是一个 git 项目的最新进展，请根据功能合并同类项，形成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题。\n你可以根据开头的标题判断出该项目是否是一个知名项目，如果是，请根据你了解到的知识判断哪些改动项是重要的，将其高亮标注出来。\n简报使用中文撰写。"
         
+        prompt = markdown_content
         if dry_run:
             # 如果启用了dry_run模式，将不会调用模型，而是将提示信息保存到文件中
             LOG.info("Dry run mode enabled. Saving prompt to file.")
@@ -25,10 +28,18 @@ class LLM:
         LOG.info("Starting report generation using GPT model.")
         
         try:
-            # 调用OpenAI GPT模型生成报告
-            response = self.client.chat.completions.create(
-                model="gpt-3.5-turbo",  # 指定使用的模型版本
+            # 调用 OpenAI 模型生成报告
+            # client = self.openai_client
+            # model = "gpt-3.5-turbo"
+
+            # 调用 智谱AI 模型生成报告
+            client = self.zhipu_client
+            model = "glm-4-0520"
+
+            response = client.chat.completions.create(
+                model=model,
                 messages=[
+                    {"role": "system", "content": system_prompt},
                     {"role": "user", "content": prompt}  # 提交用户角色的消息
                 ]
             )
